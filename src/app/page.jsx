345e85842975a7cb176e8fc6ba3418b491ea496f
luis Colina <colina.1990lz@gmail.com>
2024-03-05 12:55:10 -0400

    minor accesbility changes, workflow version update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="*:break-inside-avoid columns-[290px] gap-8 m-8 *:mb-4 max-w-[1536px] 2xl:mx-auto">
       {galleryData.map(e => <Link href={e.src} key={`${e.id}-${e.src}`}>
-        <GalleryItem url={e.src} author={e.author} />
+        <GalleryItem url={e.src} author={e.author} priority={e.id<2?true:false}/>
       </Link>)}
     </main>
   );
